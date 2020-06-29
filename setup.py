d13fa792c31674e0dd9ad4a9309d99b59b2b7245
Hyungjun Kim <hyungjun@gmail.com>
2020-06-30 07:15:44 +0900

    alpha version published to pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,22 @@
 from distutils.core import setup
 
 setup( name                 = 'gtool3',
-       version              = '0.6',
+       version              = '0.6a',
        description          = 'gtool io sub module of coreFrame',
+       long_description     = ''' long_description to be written. ''',
+       classifiers          = [
+                            'Development Status :: 4 - Beta',
+                            'License :: OSI Approved :: MIT License',
+                            'Programming Language :: Python :: 2.7',
+                            'Topic :: Scientific/Engineering :: Atmospheric Science',
+                            ],
+       keywords             = 'gtool miroc matsiro',
+       url                  = 'https://github.com/hyungjun/gtool3',
        author               = 'Hyungjun Kim',
        author_email         = 'hyungjun@gmail.com',
-       url                  = '',
-       package_dir          = {'gtool3':'./'},
-       packages             = ['gtool3'],
-       install_requires     = ['numpy'],
+       license              = 'MIT',
+
+       package_dir          = {'gtool3/gtool3':''},
+       packages             = ['gtool3', 'gtool3/filters'],
       )
+
